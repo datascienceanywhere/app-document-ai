01e7f2c88434fd79fa5e26fb06ffcacdc810f685
datascienceanywhere <datascienceanywhere@gmail.com>
2026-03-02 23:58:43 +0530

    gitignore, requirements.txt, dockerfile

--- config/settings.py
+++ config/settings.py
@@ -1,9 +1,13 @@
-# base path
-import os
-import sys
+try:
+    # base path
+    import os
+    import sys
 
-base_path = os.path.abspath('..')
-sys.path.append(base_path)
+    base_path = os.path.abspath('..')
+    sys.path.append(base_path)
 
-# import helper
-import helper
+    # import helper
+    import helper
+
+except Exception as e:
+    print("Running in cloud environment", e)
